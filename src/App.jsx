bcf7e5640b0a3bedeb2250387ec9c11e0074a32d
karthikrish06 <karthikrish066@gmail.com>
2024-08-23 22:46:21 +0530

    First React Application

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Greet/>
+    </>
+  )
+}
+function Greet() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
+  )
+}
+export default App
